1d6d84599db8718b43fea0e288abe007e6ed9a01
Khaled-ALIMI <123839989+Khaled-ALIMI@users.noreply.github.com>
2023-01-28 23:20:01 +0100

    Add files via upload

---
+++ launch/listener.launch.py
@@ -0,0 +1,10 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+	return launchDescription{[
+		Node(
+			package='demo_nodes_py',
+			executable='listener'
+		)
+	]}
